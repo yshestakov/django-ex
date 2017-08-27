501e93776688e2a57124dcbb92ef99263ef6996c
Yuriy Shestakov <yshestakov@gmail.com>
2017-08-28 01:04:17 +0300

    Made blog.post_list a home apge

--- project/urls.py
+++ project/urls.py
@@ -3,14 +3,17 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 from welcome.views import index, health
+from blog.views import post_list
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
+    url(r'^blog/', include('blog.urls')),
+    # url(r'^$', index),
+    url(r'^$', post_list),
+    url(r'^welcome$', index, name='welcome'),
+    url(r'^health$', health, name='health'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout',
